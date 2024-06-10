a2bdaeefa13506395295c2ea30d8e41f1d349eff
gaetabno <88724808+gaetabno@users.noreply.github.com>
2024-06-10 19:37:16 +0200

    todolist - create project

---
+++ src/components/AddRecord.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {recordList} from "../lib/store.js";
+
+    let value;
+    const addRecordToList = () => {
+        $recordList = [
+            ...$recordList,
+            {
+                item: value,
+                checked: false,
+            },
+        ];
+
+        value = "";
+    };
+</script>
+
+<form class="grid gap-4" on:submit|preventDefault={addRecordToList}>
+    <input
+        type="text"
+        name="record"
+        bind:value
+        placeholder="Inserisci Record*"
+        required
+        class="input input-bordered w-full"
+    />
+    <button type="submit" class="btn btn-outline btn-primary m-auto"
+        >Aggiungi</button
+    >
+</form>
---
+++ src/components/RecordList.svelte
@@ -0,0 +1,24 @@
+<script>
+    import RecordItem from "./RecordItem.svelte";
+    import {recordList, recordCheckedList} from "../lib/store.js";
+</script>
+
+<div class="grid grid-cols-1 py-5">
+    <div class="py-3">
+        <h3 class="text-lg font-bold">Record da svolgere</h3>
+
+        {#each $recordList as item (item)}
+            <RecordItem record={item} />
+        {:else}
+            non ci sono record da svolgere
+        {/each}
+    </div>
+    <div class="py-3">
+        <h3 class="text-lg font-bold">Record già fatti</h3>
+        {#each $recordCheckedList as item (item)}
+            <RecordItem record={item} />
+        {:else}
+            non ci sono record già fatti
+        {/each}
+    </div>
+</div>
